2fbf8c5507347206850b59ab5683a9f49aaa26d0
ChristopherSTAN <497592613@qq.com>
2020-08-11 21:59:13 -0400

    Update voc0712.py

--- configs/_base_/datasets/voc0712.py
+++ configs/_base_/datasets/voc0712.py
@@ -64,7 +64,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
     dict(type='Resize', img_scale=[(768, 768), (480, 480)],multiscale_mode='range',keep_ratio=True),
-    # dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='RandomFlip', flip_ratio=0.5),
     dict(
         type='Albu',
         transforms=albu_train_transforms,
